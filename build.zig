dd5ef69092938185f90336bf297dd96ae5f84157
serpens-ignis <serpens.stygius@proton.me>
2024-11-04 19:27:43 -0300

    build: create dedicated function for 'build trihack'

--- build.zig
+++ build.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+var target: std.Build.ResolvedTarget = undefined;
+var optimize: std.builtin.OptimizeMode = undefined;
 
+fn build_trihack(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "trihack",
         .root_source_file = b.path("src/main.zig"),
@@ -11,9 +11,14 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     b.installArtifact(exe);
-
     const run_cmd = b.addRunArtifact(exe);
-    const run_step = b.step("run", "Run the app");
+    const run_step = b.step("run", "Run trihack");
     run_cmd.step.dependOn(b.getInstallStep());
     run_step.dependOn(&run_cmd.step);
+}
+
+pub fn build(b: *std.Build) void {
+    target = b.standardTargetOptions(.{});
+    optimize = b.standardOptimizeOption(.{});
+    build_trihack(b);
 }
